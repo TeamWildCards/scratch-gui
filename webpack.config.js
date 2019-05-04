b86ab65c76b9b009c4020466a8dceeb4982ff24e
hexarob <rob.desoto@gmail.com>
2019-05-04 18:57:02 -0500

    Change page name to Code editor

--- webpack.config.js
+++ webpack.config.js
@@ -147,7 +147,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Wildcards GUI',
+                title: 'Code Editor',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({
